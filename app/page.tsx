166791251749ce92df749d58660f7b7cc0af0a0a
shreyas <shreyasmamtha19@gmail.com>
2026-04-17 21:58:18 +0530

    dede

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import dynamic from "next/dynamic";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import AboutPreview from "./components/AboutPreview";
 import Philosophy from "./components/Philosophy";
 import Audience from "./components/Audience";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
+
+import HeroWrapper from "./components/HeroWrapper";
 
 /* ================= SEO METADATA ================= */
 export const metadata = {
@@ -37,7 +39,7 @@ export default function Home() {
       <Navbar />
       
       <main className="bg-[#030812]">
-        <Hero />
+        <HeroWrapper />
         <AboutPreview />
         <Philosophy />
         <Audience />
